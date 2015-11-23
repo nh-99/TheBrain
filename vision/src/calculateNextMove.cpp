41cb19c5361055b06f05c22f8926ac1e110e3179
nh-99 <ifence9@gmail.com>
2015-11-23 08:56:24 -0500

    Structure changes, does not compile

---
+++ vision/src/calculateNextMove.cpp
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<string>
+
+namespace std;
+
+void getNextMove(float distanceToGoal, float widthOfRobot, float outerWheelVelocity) {
+    double innerVelocity = calculateInnerVelocity(distanceToGoal, widthOfRobot, outerWheelVelocity);
+    checkForAction(distanceToGoal);
+}
+
+double calculateInnerVelocity(float distanceToGoal, float widthOfRobot, float outerWheelVelocity) {
+    return (outerWheelVelocity * distanceToGoal/(2 * widthOfRobot * distanceToGoal));
+}
+
+string checkForAction(float distanceToGoal) {
+    if(distanceToGoal < 15) {
+        initiatePickup();
+    }
+}
+
+void initiatePickup() {
+    // TODO: Tell the robot to pickup a tote here with fancy networking shit
+}
