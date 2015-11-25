c7527aefac99b988162e3ad4aaff9ae2cd6eb3da
nh-99 <ifence9@gmail.com>
2015-11-25 00:31:09 -0500

    WIP HSV thresholding

--- vision/src/vision.cpp
+++ vision/src/vision.cpp
@@ -1,23 +1,25 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
 #include <stdlib.h>
 #include <stdio.h>
 
 using namespace cv;
 
 Mat applyHsvThreshold(Mat srcImage) {
-    // TODO: Apply HSV threshold
-    return srcImage;
+    Mat hsv_image, toReturn; // The material that the function will return
+    medianBlur(srcImage, srcImage, 3);
+    cvtColor(srcImage, hsv_image, COLOR_BGR2HSV); // TODO: only apply this to a clone, return og for other function
+    inRange(hsv_image, Scalar(86, 117, 209), Scalar(96, 255, 255), toReturn);
+    return toReturn;
 }
 
 Mat applyCannyDetect(Mat srcImage) {
-    Mat src_gray, detected_edges;
+    Mat detected_edges;
     Mat dst;
     dst.create(srcImage.size(), srcImage.type());
 
-    cvtColor(srcImage, src_gray, CV_BGR2GRAY);
-
-    blur(src_gray, detected_edges, Size(3, 3));
+    // cvtColor(srcImage, src_gray, CV_BGR2GRAY);
     /// Canny detector¬
     Canny(detected_edges, detected_edges, 100, 100 * 3, 3);
     /// Using Canny's output as a mask, we display our result¬
@@ -33,7 +35,10 @@ Mat applyImageTransformations(Mat srcImage) {
 }
 
 int main() {
-    Mat src = imread("/home/noah/Pictures/SampleImages/image13.jpg");
+    Mat src = imread("/home/noah/Pictures/SampleImages/image.jpg");
+    if(src.empty()) {
+        return 1;
+    }
     Mat testApply = applyImageTransformations(src);
     imshow("Test", testApply);
     waitKey(0);
