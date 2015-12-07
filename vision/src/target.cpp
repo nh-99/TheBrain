d4d8afb8de9e58b94045ad79286d5fa1b81d92cd
nh-99 <ifence9@gmail.com>
2015-12-07 18:16:42 -0500

    syncing workspaces

---
+++ vision/src/target.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <math.h>
+
+double distanceToRotation(double distanceToTarget) {
+    // FUCKING FIX THIS
+}
+
+double angleToRotationVertex(double distanceToTarget) {
+   //First angle to find 
+}
+
+double distanceAfterRotation(double distanceToTarget) {
+    //
+}
