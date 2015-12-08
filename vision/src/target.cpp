1d1e1409b68c5a6707754e53987e6d68e6479e50
nh-99 <ifence9@gmail.com>
2015-12-07 23:26:50 -0500

    Unit testing for algorithms & refined program structure

--- vision/src/target.cpp
+++ vision/src/target.cpp
@@ -3,13 +3,13 @@
 double Target::initialAngleToRotate(double distanceOne, double distanceTwo) {
     // cos^-1((d2-d1-x1)/(2*d1*x1)) = angleToRotate
     // distances must be in inches
-    return cos^-1(((2.75)^2 + distanceOne^2 - distanceTwo^2)/(2 * distanceOne * 2.75));
+    return acos((pow(2.75,2.0) + pow(distanceOne,2.0) - pow(distanceTwo,2.0))/(2 * distanceOne * 2.75));
 }
 
 double Target::distanceToFinalRotation(double angleToRotate, double distanceOne) {
-    return cos(angleToRotate) * distanceOne;
+    return (cos(angleToRotate) * distanceOne);
 }
 
 double Target::distanceToTote(double angleToRotate, double distanceOne) {
-    return sin(angleToRotate) * distanceOne;
+    return (sin(angleToRotate) * distanceOne);
 }
--- vision/test/TestDistanceAlgorithms.h
+++ vision/test/TestDistanceAlgorithms.h
@@ -3,20 +3,20 @@
 #include <math.h>
 
 class TestDistanceAlgorithms : public CxxTest::TestSuite {
+    Target target;
     public:
-        Target target;
         void testGetAngle(void) {
             double angle = target.initialAngleToRotate(5.385, 4.472);
-            TS_ASSERT_EQUALS(round(angle), 0.0);
+            TS_ASSERT_EQUALS(round(angle), 1.0);
         }
         void testGetDistanceToFinalRotation(void) {
             double angle = target.initialAngleToRotate(5.385, 4.472);
             double distanceToRotation = target.distanceToFinalRotation(angle, 5.385);
-            TS_ASSERT_EQUALS(round(distanceToRotation), 5.0);
+            TS_ASSERT_EQUALS(round(distanceToRotation), 3.0);
         }
         void testDistanceToTote(void) {
             double angle = target.initialAngleToRotate(5.385, 4.472);
             double distanceToTote = target.distanceToTote(angle, 5.385);
-            TS_ASSERT_EQUALS(round(distanceToTote), 2.0);
+            TS_ASSERT_EQUALS(round(distanceToTote), 4.0);
         }
 };
