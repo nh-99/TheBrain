6cd5e5525411d84eb4da692f9b35c5c4b6c8e22b
nh-99 <ifence9@gmail.com>
2015-12-07 20:29:27 -0500

    Distance and angle equations

--- vision/src/target.cpp
+++ vision/src/target.cpp
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <math.h>
 
-double distanceToRotation(double distanceToTarget) {
-    // FUCKING FIX THIS
+double initialAngleToRotate(double distanceOne, double distanceTwo) {
+    // cos^-1((d2-d1-x1)/(2*d1*x1)) = angleToRotate
+    // distances must be in inches
+    return cos^-1(((2.75)^2 + distanceOne^2 - distanceTwo^2)/(2 * distanceOne * 2.75));
 }
 
-double angleToRotationVertex(double distanceToTarget) {
-   //First angle to find 
+double distanceToFinalRotation(double angleToRotate, double distanceOne) {
+    return cos(angleToRotate) * distanceOne;
 }
 
-double distanceAfterRotation(double distanceToTarget) {
-    //
+double distanceToTote(double angleToRotate, double distanceOne) {
+    return sin(angleToRotate) * distanceOne;
 }
