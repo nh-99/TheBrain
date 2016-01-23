c2844bbab36c93cb9f60c4f0655e5976f1a457c9
nh-99 <ifence9@gmail.com>
2016-01-23 12:03:05 -0500

    Updates to vision functions, adds the steps to process image

--- vision/src/vision.h
+++ vision/src/vision.h
@@ -3,11 +3,17 @@
 #include <opencv2/opencv.hpp>
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
+#include <map>
 
 using namespace cv;
 
 class Vision {
-    public:
+    private:
         Mat applyHsvThreshold(Mat srcImage);
-        Mat applyImageTransformations(Mat srcImage);
+        Mat applyConvexHull(Mat srcImage);
+        Mat applyMinusThreshold(Mat srcImage);
+        vector getImageContours(Mat srcImage);
+    public:
+        map getValues(vector imageContours);
 };
