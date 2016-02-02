d64abf48c862c259f58c6ed056cb2d0b9fe81cd2
nh-99 <ifence9@gmail.com>
2016-02-02 09:44:51 -0500

    Fixes for distance equation

--- vision/src/vision.cpp
+++ vision/src/vision.cpp
@@ -5,6 +5,8 @@ RNG rng(12345);
 int max_thresh = 255;
 int w_filter = 30;
 
+double getHeight(double, double, double, double);
+
 Mat Vision::applyHsvThreshold(Mat srcImage) {
     Mat hsv_image, threshold, toReturn; // The material that the function will return
     medianBlur(srcImage, srcImage, 3);
@@ -28,8 +30,6 @@ vector<RotatedRect> Vision::getContours(Mat srcImage) {
     threshold(srcImage, threshold_output, thresh, 255, THRESH_BINARY);
     /// Find contours
     findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-    /// Approximate contours to polygons + get bounding rects and circles
     vector<RotatedRect> minRect(contours.size());
 
     for(int i = 0; i < contours.size(); i++) {
@@ -43,13 +43,19 @@ vector<map<int, double> > Vision::getResults(vector<RotatedRect> zones) {
     for(int i = 0; i < zones.size(); i++) {
         if(zones[i].size.width > w_filter) { // Filter the objects by width before returning the values
             map<int, double> zoneValues;
+            Point2f rectPoints[4];
+            zones[i].points(rectPoints);
             zoneValues[0] = zones[i].size.width;
             zoneValues[1] = zones[i].size.height;
-//            zoneValues[2] = zones[i].size().area();
-//            zoneValues[3] = ((zones[i].x + zones[i].width) / 2);
-//            zoneValues[4] = ((zones[i].y + zones[i].height) / 2);
+            zoneValues[2] = zones[i].size.area();
+            zoneValues[3] = getHeight(rectPoints[0].x, rectPoints[1].x, rectPoints[0].y, rectPoints[1].y);
+            zoneValues[4] = getHeight(rectPoints[2].x, rectPoints[3].x, rectPoints[2].y, rectPoints[3].y);
             toReturn.push_back(zoneValues);
         }
     }
     return toReturn;
+}
+
+double getHeight(double x1, double x2, double y1, double y2) {
+    return sqrt(pow(x2-x1,2) + pow(y2-y1,2));
 }
